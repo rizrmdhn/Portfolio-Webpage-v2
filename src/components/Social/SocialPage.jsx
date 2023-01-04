046b6f1507d4e8889102b3bcd0c3ae661881efe0
rizrmdhn <rizrmdhn.work@gmail.com>
2023-01-04 16:39:08 +0800

    add social page

--- src/components/Social/SocialPage.jsx
+++ src/components/Social/SocialPage.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./styles/styles.css";
 
 export default function SocialPage() {
   return (
-    <div className='animate__animated animate__fadeIn'>SocialPage</div>
-  )
+    <div className="social-container animate__animated animate__fadeIn">
+      <div className="social-link">
+        <a
+          className="twitter-link"
+          href="https://twitter.com/AzzezEd"
+          target="_blank"
+        >
+          <i class="bi bi-twitter"></i>
+          Twitter
+        </a>
+        <a
+          className="github-link"
+          href="https://github.com/rizrmdhn"
+          target="_blank"
+        >
+          <i class="bi bi-github"></i>
+          Github
+        </a>
+      </div>
+    </div>
+  );
 }
